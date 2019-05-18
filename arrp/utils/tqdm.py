b3fd555de896b82955ddabcf0989325696f31f52
Luca Cappelletti <cappelletti.luca94@gmail.com>
2019-05-18 19:25:24 +0200

    Introduced task building parallelization

--- arrp/utils/tqdm.py
+++ arrp/utils/tqdm.py
@@ -1,4 +1,4 @@
-from tqdm import tqdm as cli_tqdm, tqdm_notebook
+from tqdm import tqdm as tqdm_cli, tqdm_notebook
 from .is_notebook import is_notebook
 
-tqdm = tqdm_notebook if is_notebook() else cli_tqdm
+tqdm = tqdm_notebook if is_notebook() else tqdm_cli
