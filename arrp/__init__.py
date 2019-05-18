aebf76499e821d983e80d490147a86d9295e2b38
Luca Cappelletti <cappelletti.luca94@gmail.com>
2019-05-18 16:23:07 +0200

    Fixed a number of errors

--- arrp/__init__.py
+++ arrp/__init__.py
@@ -2,5 +2,7 @@ from .build import build
 from .load import load
 from .clear import clear
 from .utils import get_cell_lines
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
 
 __all__ = ["build", "load", "clear", "get_cell_lines"]
--- arrp/tasks/build_tasks.py
+++ arrp/tasks/build_tasks.py
@@ -20,7 +20,7 @@ def load_nucleotides_sequences(target:str, cell_line:str):
             k=1,
         ), 
         index_col=0
-    ).values.reshape(-1, 200, 5)
+    )
 
 def load_classes(target:str, cell_line:str):
     return pd.read_csv(
@@ -31,14 +31,15 @@ def load_classes(target:str, cell_line:str):
         index_col=0
     )
 
-def drop_unknown(cellular_variables:pd.DataFrame, nucleotides_sequences:pd.DataFrame, classes:pd.DataFrame)->Tuple:
+def drop_unknown(cellular_variables:pd.DataFrame, nucleotides_sequences:pd.DataFrame, nucleotides_sequences_index:pd.DataFrame, classes:pd.DataFrame)->Tuple:
     """Remove datapoints labeles as UK."""
     unknown = classes["UK"] == 1
     cellular_variables = cellular_variables.drop(index=cellular_variables.index[unknown])
     nucleotides_sequences = nucleotides_sequences[~unknown]
+    nucleotides_sequences_index = nucleotides_sequences_index[~unknown]
     classes = classes.drop(index=classes.index[unknown])
     classes = classes.drop(columns=["UK"])
-    return cellular_variables, nucleotides_sequences, classes
+    return cellular_variables, nucleotides_sequences, nucleotides_sequences_index, classes
 
 @mkdir
 def get_cell_line_path(path:str, cell_line:str):
@@ -46,14 +47,17 @@ def get_cell_line_path(path:str, cell_line:str):
 
 @mkdir
 def get_task_path(path:str, task:str):
-    return "{path}/{task}".format(path=path, task=task.replace(" ", "_"))
+    return "{path}/tasks/{task}".format(path=path, task=task.replace(" ", "_"))
 
 def build_tasks(target:str, tasks:List, holdouts:int, validation_split:float, test_split:float, balance_settings:Dict):
     for cell_line in tqdm(get_cell_lines(target), desc="Cell lines"):
         cellular_variables = load_cellular_variables(target, cell_line)
         nucleotides_sequences = load_nucleotides_sequences(target, cell_line)
+        nucleotides_sequences_index = nucleotides_sequences.index.values.reshape(-1, 200)
+        nucleotides_sequences_header = nucleotides_sequences.columns
+        nucleotides_sequences = nucleotides_sequences.values.reshape(-1, 200, 5)
         classes = load_classes(target, cell_line)
-        cellular_variables, nucleotides_sequences, classes = drop_unknown(cellular_variables, nucleotides_sequences, classes)
+        cellular_variables, nucleotides_sequences, nucleotides_sequences_index, classes = drop_unknown(cellular_variables, nucleotides_sequences, nucleotides_sequences_index, classes)
         cell_line_path = get_cell_line_path(target, cell_line)
         for task in tqdm([task for task in tasks if any(task["balancing"].values())], desc="Building tasks"):
             build_task(
@@ -65,5 +69,7 @@ def build_tasks(target:str, tasks:List, holdouts:int, validation_split:float, te
                 test_split,
                 cellular_variables,
                 nucleotides_sequences,
+                nucleotides_sequences_index,
+                nucleotides_sequences_header,
                 classes
             )
