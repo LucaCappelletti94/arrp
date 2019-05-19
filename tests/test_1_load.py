fb176b918efa4ee89f1c3fc01e3dea9c31bf4b47
Luca Cappelletti <cappelletti.luca94@gmail.com>
2019-05-19 12:43:51 +0200

    Cleared test

--- tests/test_1_load.py
+++ tests/test_1_load.py
@@ -1,9 +1,11 @@
 from arrp import load_validation, holdouts_generator, tasks_generator
-from arrp.utils import tqdm
+from arrp.utils import tqdm, load_settings
 
 def test_load():
-    for task in tqdm(list(tasks_generator("test_dataset")), desc="Jobs"):
+    target = "test_dataset"
+    holdouts = load_settings(target)["holdouts"]
+    for task in tqdm(list(tasks_generator(target)), desc="Jobs"):
         load_validation(*task)
         [
-            _ for _ in tqdm(holdouts_generator(*task), desc="Holdouts")
+            _ for _ in tqdm(holdouts_generator(*task), desc="Holdouts", total=holdouts)
         ]
