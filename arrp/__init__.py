8304c40294692bc987856d5325c281761006c80c
Luca Cappelletti <cappelletti.luca94@gmail.com>
2019-05-19 10:58:44 +0200

    major patch

--- arrp/__init__.py
+++ arrp/__init__.py
@@ -1,6 +1,4 @@
 from .build import build
-from .load import load
-from .clear import clear
 from .utils import get_cell_lines
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
---
+++ arrp/input/__init__.py
@@ -0,0 +1,3 @@
+from .input import build_input
+
+__all__ = ["build_input"]
