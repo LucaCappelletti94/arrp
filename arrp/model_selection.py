4015099b34e0cf2885b998f2afcc22f0d549fd29
Luca Cappelletti <cappelletti.luca94@gmail.com>
2019-05-30 13:36:15 +0200

    Included automatic reporting with notipy

--- arrp/model_selection.py
+++ arrp/model_selection.py
@@ -15,6 +15,7 @@ from auto_tqdm import tqdm
 from plot_keras_history import plot_history
 import matplotlib.pyplot as plt
 import matplotlib
+from notipy_me import Notipy
 matplotlib.use('Agg')
 
 def dict_holdout_generator(generator)->Generator:
@@ -52,7 +53,7 @@ def is_model_cached(path: str, best_parameters: Dict)->bool:
     return False
 
 
-def save_results(model: Model, history: Dict, best_parameters: Dict, path: str):
+def save_results(model: Model, history: Dict, best_parameters: Dict, path: str, N:Notipy):
     os.makedirs(path, exist_ok=True)
     hash_path = "{path}/hash.json".format(path=path)
     with open(hash_path, "w") as f:
@@ -62,9 +63,11 @@ def save_results(model: Model, history: Dict, best_parameters: Dict, path: str):
     model.save("{path}/model.h5".format(path=path))
     plot_history(history)
     plt.savefig("{path}/history.png".format(path=path))
-    pd.DataFrame(history).to_csv("{path}/history.csv".format(path=path))
+    df = pd.DataFrame(history)
+    df.to_csv("{path}/history.csv".format(path=path))
     with open("{path}/best_parameters.json".format(path=path), "w") as f:
         json.dump(best_parameters, f, indent=4)
+    N.add_report(df[["auprc", "val_auprc"]].tail(1))
 
 
 def collect_results(path: str, holdouts: int):
@@ -77,32 +80,33 @@ def collect_results(path: str, holdouts: int):
 
 
 def model_selection(target: str, name: str, structure: Callable, space: Space):
-    settings = load_settings(target)
-    for task in tqdm(list(tasks_generator(target)), desc="Tasks"):
-        generator = dict_holdout_generator(balanced_holdouts_generator(*task))
-        path = get_path(name, *task)
-        for i, ((training, testing), inner_holdouts) in enumerate(generator()):
-            mlp_space["input_shape"] = (training[0]["mlp"].shape[1],)
-            tuner = ModelTuner(structure, space, inner_holdouts,
-                               settings["training"], settings["gaussian_process"]["monitor"])
-            best_parameters = tuner.tune(
-                cache_dir="{path}/{i}/.gaussian_cache".format(path=path, i=i),
-                callback=[
-                    TQDMGaussianProcess(
-                        n_calls=settings["gaussian_process"]["tune"]["n_calls"]),
-                    DeltaYStopper(
-                        **settings["gaussian_process"]["early_stopping"])
-                ],
-                **settings["gaussian_process"]["tune"]
-            )
-            if not is_model_cached("{path}/{i}".format(path=path, i=i), best_parameters):
-                best_model = model(
-                    *structure(**best_parameters),
-                    metrics=["auprc", "auroc", "accuracy", "false_negatives", "false_positives",
-                             "true_negatives", "true_positives", "precision", "recall"]
+    with Notipy() as N:
+        settings = load_settings(target)
+        for task in tqdm(list(tasks_generator(target)), desc="Tasks"):
+            generator = dict_holdout_generator(balanced_holdouts_generator(*task))
+            path = get_path(name, *task)
+            for i, ((training, testing), inner_holdouts) in enumerate(generator()):
+                mlp_space["input_shape"] = (training[0]["mlp"].shape[1],)
+                tuner = ModelTuner(structure, space, inner_holdouts,
+                                settings["training"], settings["gaussian_process"]["monitor"])
+                best_parameters = tuner.tune(
+                    cache_dir="{path}/{i}/.gaussian_cache".format(path=path, i=i),
+                    callback=[
+                        TQDMGaussianProcess(
+                            n_calls=settings["gaussian_process"]["tune"]["n_calls"]),
+                        DeltaYStopper(
+                            **settings["gaussian_process"]["early_stopping"])
+                    ],
+                    **settings["gaussian_process"]["tune"]
                 )
-                history = fit(training, testing, best_model,
-                              settings["training"])
-                save_results(best_model, history, best_parameters,
-                             "{path}/{i}".format(path=path, i=i))
-        collect_results(path, settings["holdouts"]["quantities"][0])
+                if not is_model_cached("{path}/{i}".format(path=path, i=i), best_parameters):
+                    best_model = model(
+                        *structure(**best_parameters),
+                        metrics=["auprc", "auroc", "accuracy", "false_negatives", "false_positives",
+                                "true_negatives", "true_positives", "precision", "recall"]
+                    )
+                    history = fit(training, testing, best_model,
+                                settings["training"])
+                    save_results(best_model, history, best_parameters,
+                                "{path}/{i}".format(path=path, i=i), N)
+            collect_results(path, settings["holdouts"]["quantities"][0])
