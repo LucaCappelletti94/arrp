802156dd39172497527c0c7608da88b936c34027
Luca Cappelletti <cappelletti.luca94@gmail.com>
2019-06-01 17:04:45 +0200

    Finished removing code smells

--- arrp/clear.py
+++ arrp/clear.py
@@ -2,7 +2,8 @@ import shutil
 import os
 from holdouts_generator import clear_holdouts_cache
 
-def clear(target:str):
+
+def clear(target: str):
     dirs = (
         "one_hot_encoded_classes",
         "one_hot_encoded_expanded_regions",
@@ -11,13 +12,14 @@ def clear(target:str):
     for d in dirs:
         shutil.rmtree(
             "{target}/{d}".format(target=target, d=d), ignore_errors=True
-        ) 
-    
+        )
+
     gzip_dir = "{target}/epigenomic_data".format(target=target)
     for document in os.listdir(gzip_dir):
         if document.endswith(".csv"):
             os.remove(
-                "{gzip_dir}/{document}".format(gzip_dir=gzip_dir, document=document)
-            )  
+                "{gzip_dir}/{document}".format(gzip_dir=gzip_dir,
+                                               document=document)
+            )
 
     clear_holdouts_cache(".holdouts")
