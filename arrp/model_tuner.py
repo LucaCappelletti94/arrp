8227d6bc5bc4395b2b66e854ecfdcfa44624500f
Luca Cappelletti <cappelletti.luca94@gmail.com>
2019-06-07 14:46:42 +0200

    Brought caching to single holdout level.

--- arrp/model_tuner.py
+++ arrp/model_tuner.py
@@ -6,6 +6,7 @@ from .model_fit import fit
 from gaussian_process import GaussianProcess, Space
 from typing import Callable, Dict
 from keras.backend import clear_session
+from dict_hash import sha256
 import gc
 
 class ModelTuner:
@@ -23,17 +24,35 @@ class ModelTuner:
         compiled_model = model(*self._structure(**structure))
         weights = compiled_model.get_weights()
         scores = []
+        parameters_hash = sha256({
+            "structure":structure,
+            "training":training,
+            "data_hash":data_hash
+        })
         for (training_set, testing_set), _ in self._holdouts():
-            compiled_model.set_weights(weights)
-            scores.append(self._calculate_score({
-                k: v[-1] for k, v in fit(training_set, testing_set, compiled_model, training).items()
-            }))
+            path = "{cache_dir}/{parameters_hash}/{holdout_hash}".format(
+                cache_dir=self._cache_dir,
+                parameters_hash=parameters_hash,
+                holdout_hash=sha256({
+                    "training_set":training_set,
+                    "testing_set":testing_set
+                })
+            )
+            history_path = "{path}/history.csv".format(path=path)
+            if not os.path.exists(history_path):
+                os.makedirs(path, exist_ok=True)
+                compiled_model.set_weights(weights)
+                history = pd.DataFrame(fit(training_set, testing_set, compiled_model, training))
+                history.index.name = "Epochs"
+                history.to_csv(history_path)
+            history = pd.read_csv(history_path, index_col="Epochs")
+            scores.append(self._calculate_score(history.iloc[-1].to_dict()))
         clear_session()
         gc.collect()
         return -np.mean(scores)
 
     def tune(self, cache_dir: str, **kwargs) -> Dict:
         self._cache_dir = cache_dir
-        gp = GaussianProcess(self._score, self._space, cache_dir=cache_dir)
+        gp = GaussianProcess(self._score, self._space, cache=False)
         gp.minimize(**kwargs)
         return gp.best_parameters
