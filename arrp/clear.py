8077b4f30c17a24c0031c88884f131734b0947d7
Luca Cappelletti <cappelletti.luca94@gmail.com>
2019-06-30 08:27:14 +0200

    Fixed an import error

--- arrp/clear.py
+++ arrp/clear.py
@@ -1,6 +1,6 @@
 import shutil
 import os
-from holdouts_generator import clear_cache, clear_memory_cache
+from holdouts_generator import clear_cache
 
 
 def clear(target: str):
@@ -23,4 +23,3 @@ def clear(target: str):
             )
 
     clear_cache(".holdouts")
-    clear_memory_cache()
